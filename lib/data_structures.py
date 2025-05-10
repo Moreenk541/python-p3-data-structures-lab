5f76db105ee12f132c798f2f65d3541fc6a61ecf
moreen karimi <moreenk541@gmail.com>
2025-05-10 18:45:46 +0300

    data structures

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,37 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names =[key["name"] for key in spicy_foods]
+    return names
+print(get_names)
+
+
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+         heat_emoji = "🌶"* food["heat_level"]
+         print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_emoji}")
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+   return next(food for food in spicy_foods if food["cuisine"] == cuisine)
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+     for food in get_spiciest_foods(spicy_foods):  # reuse the filtering function
+        heat_emoji = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_emoji}")
+    
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat = sum(food["heat_level"] for food in spicy_foods)
+    count = len(spicy_foods)
+    return total_heat // count  # Return the average heat level as an integer
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
